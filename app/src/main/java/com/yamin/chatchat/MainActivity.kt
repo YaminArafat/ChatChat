88b605caa6e2f22f166cfdfc6ccb0d82b9ebc6b8
YaminArafat <yaminarafat032@gmail.com>
2023-03-12 02:53:36 +0600

    Basic welcome UI build

--- app/src/main/java/com/yamin/chatchat/MainActivity.kt
+++ app/src/main/java/com/yamin/chatchat/MainActivity.kt
@@ -2,10 +2,18 @@ package com.yamin.chatchat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.yamin.chatchat.fragments.AppWelcomeScreenFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setWelcomeScreenFragment()
+    }
+
+    private fun setWelcomeScreenFragment(){
+        val welcomeScreenFragment = AppWelcomeScreenFragment()
+        supportFragmentManager.beginTransaction().add(R.id.main_activity_fragment_container, welcomeScreenFragment).commit()
     }
 }
