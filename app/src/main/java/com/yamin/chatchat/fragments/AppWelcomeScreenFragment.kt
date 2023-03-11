e4b34f5537e401ce5b1303bdc564552a6edb72cc
YaminArafat <yaminarafat032@gmail.com>
2023-03-12 02:53:36 +0600

    Basic welcome UI build

--- app/src/main/java/com/yamin/chatchat/fragments/AppWelcomeScreenFragment.kt
+++ app/src/main/java/com/yamin/chatchat/fragments/AppWelcomeScreenFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.yamin.chatchat.R
 import com.yamin.chatchat.databinding.FragmentAppWelcomeScreenBinding
 
 
@@ -27,6 +28,33 @@ class AppWelcomeScreenFragment : Fragment() {
     ): View? {
         _viewBinding = FragmentAppWelcomeScreenBinding.inflate(inflater, container, false)
         return viewBinding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewBinding?.logInButton?.setOnClickListener {
+            launchLogInFragment()
+        }
+        viewBinding?.signUpButton?.setOnClickListener {
+            launchSignUpFragment()
+        }
+    }
+
+    private fun launchSignUpFragment() {
+        val signUpFragment = SignUpFragment()
+        val fragmentManager = requireActivity().supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction().replace(R.id.main_activity_fragment_container, signUpFragment)
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.commit()
+    }
+
+    private fun launchLogInFragment() {
+        val logInFragment = LogInFragment()
+        val fragmentManager = requireActivity().supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction().replace(R.id.main_activity_fragment_container, logInFragment)
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.commit()
     }
 
     override fun onDestroyView() {
